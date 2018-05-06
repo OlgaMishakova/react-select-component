6ef00d2676515b48e45a15071dd5861e8aaf2ebb
Julia Volkova <julia.volkova789@gmail.com>
2018-05-06 23:41:38 +0300

    feat: Add highlighting for matches

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import './style.styl';
 
 class Select extends Component {
@@ -9,6 +9,7 @@ class Select extends Component {
         this.handleBlur = this.handleBlur.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.filterSuggestion = this.filterSuggestion.bind(this);
+        this.highlightMatches = this.highlightMatches.bind(this);
         this.show = this.show.bind(this);
         this.hide = this.hide.bind(this);
 
@@ -46,20 +47,47 @@ class Select extends Component {
         return suggestion.toLowerCase().includes((this.state.value.toLowerCase()));
     }
 
+    highlightMatches(suggestion) {
+        const { value } = this.state;
+        if(value.length !== 0) {
+            const start = suggestion.toLowerCase().indexOf(value.toLowerCase());
+            const prefix = suggestion.substring(0, start);
+            const match = suggestion.substring(start, start + value.length);
+            const postfix = suggestion.substring(start + value.length);
+            return <Fragment>{prefix}<strong>{match}</strong>{postfix}</Fragment>
+        } else {
+            return suggestion;
+        }
+    }
+
     renderSuggestions() {
-        const sortedSuggestions = this.props.values.slice().filter(this.filterSuggestion).sort();
+        const sortedSuggestions = this.props.values
+            .slice()
+            .filter(this.filterSuggestion)
+            .sort()
+            .map(this.highlightMatches);
+
         return sortedSuggestions.length !== 0
             ? <ul
                 className={`field__suggestions ${this.state.listVisible ? "field__suggestions--visible" : "" }`}>
-                {sortedSuggestions.map((suggestion, i) => <li key={i}>{suggestion}</li>)}
+                {
+                    sortedSuggestions.map((suggestion, i) =>
+                        <li className="field__suggestion" key={i}>{suggestion}</li>
+                    )
+                }
             </ul>
-            : <p>No matches found</p>
+            : <p className={`field__message ${this.state.listVisible ? "field__message--visible" : "" }`}>
+                No matches found
+            </p>
     }
 
     render() {
         return (
-            <div onClick={this.show}>
-                <div className="field" onFocus={this.handleFocus} onBlur={this.handleBlur}>
+            <div onClick={this.show} className="field__container">
+                <div className={`field ${this.state.listVisible ? "list-visible" : "" }`}
+                     onFocus={this.handleFocus}
+                     onBlur={this.handleBlur}
+                >
                     <input
                         type="text"
                         name="awesome-input"
